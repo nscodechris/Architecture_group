78df747c233e2cc85cb835898a80549c6a45d135
nscodechris <christoffer.vdmab@gmail.com>
2022-07-11 14:20:06 +0200

    folder_structure

--- Architect_star_wars.py
+++ Architect_star_wars.py
@@ -7,18 +7,24 @@ import glob
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 r = requests.get('https://swapi.dev/api/')
 json_data = r.json()
-
+# Data dir is where the original data is stored
+raw_data = CURR_DIR_PATH + "/raw/"
+# Target dir is where the transformed data is stored
+harmonized_data = CURR_DIR_PATH + "/harmonized/"
 
 
 def get_data(data):
     for key, value in data.items():
         q = requests.get(value)
         df = pd.DataFrame(q)
-        df.to_csv(CURR_DIR_PATH + "/" + key + ".csv", index=False)
-        print(df)
+        df.to_csv(raw_data + key + ".csv", index=False)
+        # print(df)
 
 
 get_data(json_data)
+
+
+
 
 
 
