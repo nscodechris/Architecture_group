38bcbef66a94cb135eab86f6eb7212c7a1975b3a
nscodechris <christoffer.vdmab@gmail.com>
2022-07-11 16:38:21 +0200

    new_version_film_fixed

--- Architect_star_wars.py
+++ Architect_star_wars.py
@@ -3,7 +3,10 @@ import requests
 import pandas as pd
 import os
 import glob
+import json
+import re
 
+pd.options.display.max_columns = None
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 r = requests.get('https://swapi.dev/api/')
 json_data = r.json()
@@ -11,19 +14,35 @@ json_data = r.json()
 raw_data = CURR_DIR_PATH + "/raw/"
 # Target dir is where the transformed data is stored
 harmonized_data = CURR_DIR_PATH + "/harmonized/"
+# print(json_data)
 
 
-def get_data(data):
+def get_data_raw(data):
     for key, value in data.items():
         q = requests.get(value)
-        df = pd.DataFrame(q)
-        df.to_csv(raw_data + key + ".csv", index=False)
-        # print(df)
+        result_data = q.json()["results"]
+        if key == "films":
+            df = pd.DataFrame(result_data)
+            p = re.compile(r'\r\n')
+            df['opening_crawl'] = [p.sub('', x) for x in df['opening_crawl']]
+            print(df)
+            # df["opening_crawl"].str.replace("\r", "")
+            # df["opening_crawl"].str.replace("\n", "")
+            df.to_csv(raw_data + key + ".csv", index=False)
+        else:
+            # print(result_data)
+            df = pd.DataFrame(result_data)
+            df.to_csv(raw_data + key + ".csv", index=False)
+            # print(df)
 
 
-get_data(json_data)
+get_data_raw(json_data)
 
+def get_data_harmonized():
+    df = pd.read_csv(raw_data + "films.csv")
+    print(df)
 
+# get_data_harmonized()
 
 
 
